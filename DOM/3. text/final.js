3cfc94034391954b88d9442389dd32ce08704fd1
longvoct <volonglqd.com>
2022-06-16 17:16:57 +0700

    Update Source Code

---
+++ DOM/3. text/final.js
@@ -0,0 +1,10 @@
+//1.textContent
+const spinner = document.querySelector("#spinner");
+console.log(spinner.textContent);
+//lấy tất cả các text có trong thẻ trên cho dù có một thẻ nào chèn trong, lấy tất. display:none thì vẫn hiện
+//2.innerText
+console.log(spinner.innerText);
+// lệnh này xóa tất cả các khoảng trắng, cho text nằm trên một dòng. display:none thì ko hiện
+//3.innerHTML
+spinner.innerHTML = `<div class="demo">Hello HTML</div>`;
+console.log(spinner.innerHTML);
---
+++ DOM/4. property for Class/app.js
@@ -0,0 +1,4 @@
+//1. selector.classList.add("abc")
+
+const h1Style = document.querySelector("h1");
+h1Style.classList.add("is-active");
