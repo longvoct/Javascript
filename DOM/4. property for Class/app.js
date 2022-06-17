c20a55f52df171451b779c362cda43702605777c
longvoct <volonglqd.com>
2022-06-17 23:52:39 +0700

    Update code

--- DOM/4. property for Class/app.js
+++ DOM/4. property for Class/app.js
@@ -1,4 +1,12 @@
 //1. selector.classList.add("abc")
-
 const h1Style = document.querySelector("h1");
 h1Style.classList.add("is-active");
+
+//2.selector.classList.remove("abc")
+h1Style.classList.remove("is-active");
+
+//3.selector.classList.contains("container")
+
+//4. className
+const select = document.querySelector(".container");
+console.log(select.className);
