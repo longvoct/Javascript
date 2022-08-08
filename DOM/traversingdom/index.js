e52646793f6031e63d898d774990937bd1d35d9d
longvoct <volonglqd.com>
2022-08-08 14:04:58 +0700

    update code

---
+++ DOM/traversingdom/index.js
@@ -0,0 +1,16 @@
+//1. parentNode, parentElement, removeChild
+const span = document.querySelector("span");
+/* console.log(span.parentElement);
+console.log(span.parentNode);
+console.log(span.parentNode.parentNode); */
+// span.parentNode.removeChild(span);
+//span.remove
+//2.nextElementSibling vs previousElementSibling: chọn đến phần tử kế tiếp hoặc đứng sau nó
+const nextSpan = span.nextElementSibling;
+console.log(nextSpan);
+//3.childNodes: trả về (1 mảng)hết các node bên trong bao gồm textNode vs children trả về các node không bao gồm textNode --> children[index]
+// --> thằng trả về NodeList thằng trả về HTMLCollection
+//4. firstChild vs firstElementChild
+//--> trả về phần tử đầu tiên
+//5. lastChild vs lastElementChild
+//6.nextSibling, previousSibling (có text)
