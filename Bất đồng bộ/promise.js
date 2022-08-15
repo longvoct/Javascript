5c166da14e93dd60f5bf456a56fa0658bd7c7c38
longvoct <volonglqd.com>
2022-08-15 14:32:19 +0700

    update promise

--- Bất đồng bộ/promise.js
+++ Bất đồng bộ/promise.js
@@ -23,7 +23,42 @@ function timeOut(ms) {
 }
 
 async function run() {
-  console.log(running);
+  console.log("running");
   await timeOut(2000);
-  console.log(ending);
+  console.log("ending");
+  console.log();
 }
+try {
+  run();
+} catch (err) {
+  console.log(err);
+}
+
+//arrow function
+const fn = async () => {
+  const student = {
+    fullName: async function () {
+      "";
+    },
+    async callAge() {},
+    yearly: async () => {},
+  };
+};
+
+//bắt lỗi cho async await
+//1. return rồi dùng ().then().catch((err)=>console.log(err);)
+//2. try catch trong async await
+function test(obj) {
+  return obj;
+}
+
+async function user() {
+  try {
+    const dev1 = await test("hi");
+    console.log(dev1);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(run());
